9e787a350ed54a990291bb90ca589e102c7dde2c
TGS963 <ghoshsuvojit2012@gmail.com>
2024-04-06 21:15:55 +0530

    webp image windows fix

--- src/webp_image.h
+++ src/webp_image.h
@@ -7,9 +7,9 @@
 #include "webp/decode.h"
 #include "webp/encode.h"
 
-unsigned char* webp_load(const unsigned char* buffer, int len, int* w, int* h, int* c)
+unsigned char *webp_load(const unsigned char *buffer, int len, int *w, int *h, int *c)
 {
-    unsigned char* pixeldata = 0;
+    unsigned char *pixeldata = 0;
 
     WebPDecoderConfig config;
     WebPInitDecoderConfig(&config);
@@ -21,7 +21,7 @@ unsigned char* webp_load(const unsigned char* buffer, int len, int* w, int* h, i
     int height = config.input.height;
     int channels = config.input.has_alpha ? 4 : 3;
 
-    pixeldata = (unsigned char*)malloc(width * height * channels);
+    pixeldata = (unsigned char *)malloc(width * height * channels);
 
 #if _WIN32
     config.output.colorspace = channels == 4 ? MODE_BGRA : MODE_BGR;
@@ -48,57 +48,58 @@ unsigned char* webp_load(const unsigned char* buffer, int len, int* w, int* h, i
 }
 
 #if _WIN32
-int webp_save(const wchar_t* filepath, int w, int h, int c, const unsigned char* pixeldata)
+int webp_save(const wchar_t *filepath, int w, int h, int c, const unsigned char *pixeldata, int quality)
 #else
-int webp_save(const char* filepath, int w, int h, int c, const unsigned char* pixeldata, int quality)
+int webp_save(const char *filepath, int w, int h, int c, const unsigned char *pixeldata, int quality)
 #endif
 {
     int ret = 0;
 
-    unsigned char* output = 0;
+    unsigned char *output = 0;
     size_t length = 0;
 
-    FILE* fp = 0;
+    FILE *fp = 0;
 
     if (quality >= 100)
     {
         if (c == 3)
         {
-            #if _WIN32
-                length = WebPEncodeLosslessBGR(pixeldata, w, h, w * 3, &output);
-            #else
-                length = WebPEncodeLosslessRGB(pixeldata, w, h, w * 3, &output);
-            #endif
+#if _WIN32
+            length = WebPEncodeLosslessBGR(pixeldata, w, h, w * 3, &output);
+#else
+            length = WebPEncodeLosslessRGB(pixeldata, w, h, w * 3, &output);
+#endif
         }
         else if (c == 4)
         {
-            #if _WIN32
-                length = WebPEncodeLosslessBGRA(pixeldata, w, h, w * 4, &output);
-            #else
-                length = WebPEncodeLosslessRGBA(pixeldata, w, h, w * 4, &output);
-            #endif
+#if _WIN32
+            length = WebPEncodeLosslessBGRA(pixeldata, w, h, w * 4, &output);
+#else
+            length = WebPEncodeLosslessRGBA(pixeldata, w, h, w * 4, &output);
+#endif
         }
         else
         {
             // unsupported channel type
         }
     }
-    else {
+    else
+    {
         if (c == 3)
         {
-            #if _WIN32
-                length = WebPEncodeBGR(pixeldata, w, h, w * 3, &output);
-            #else
-                length = WebPEncodeRGB(pixeldata, w, h, w * 3, quality, &output);
-            #endif
+#if _WIN32
+            length = WebPEncodeBGR(pixeldata, w, h, w * 3, &output);
+#else
+            length = WebPEncodeRGB(pixeldata, w, h, w * 3, quality, &output);
+#endif
         }
         else if (c == 4)
         {
-            #if _WIN32
-                length = WebPEncodeBGRA(pixeldata, w, h, w * 4, &output);
-            #else
-                length = WebPEncodeRGBA(pixeldata, w, h, w * 4, quality, &output);
-            #endif
+#if _WIN32
+            length = WebPEncodeBGRA(pixeldata, w, h, w * 4, &output);
+#else
+            length = WebPEncodeRGBA(pixeldata, w, h, w * 4, quality, &output);
+#endif
         }
         else
         {
@@ -122,8 +123,10 @@ int webp_save(const char* filepath, int w, int h, int c, const unsigned char* pi
     ret = 1;
 
 RETURN:
-    if (output) WebPFree(output);
-    if (fp) fclose(fp);
+    if (output)
+        WebPFree(output);
+    if (fp)
+        fclose(fp);
 
     return ret;
 }
